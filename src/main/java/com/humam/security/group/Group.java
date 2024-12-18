b64aeac7bcac07395e7e12e132a7d45443f3a712
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-18 15:57:42 +0300

    groups table

---
+++ src/main/java/com/humam/security/group/Group.java
@@ -0,0 +1,39 @@
+package com.humam.security.group;
+
+import com.humam.security.user.User;
+import com.humam.security.file.File;
+import com.humam.security.invite.Invite;
+import com.humam.security.groupmember.GroupMember;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "groups")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 55)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "created_by", nullable = false)
+    private User createdBy;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<File> files;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<GroupMember> groupMembers;
+
+    @OneToMany(mappedBy = "group", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Invite> invites;
+}

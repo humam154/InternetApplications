549e31c49eb2ba0f83a7ffe8c1cde6c4d473b8e5
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-25 22:36:22 +0300

    navigation bar: initial

---
+++ front-end/source-safe/src/Components/navigation/sideNav.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg"
+import { MdGroup, MdSettings, MdArrowBack, MdArrowForward } from "react-icons/md";
+import styles from './sideNav.module.css';
+
+interface TileProps {
+    title: string;
+    icon: React.ReactNode;
+}
+
+const Tile = ({ title, icon, open }: TileProps & { open: boolean }) => {
+    return (
+        <div className={open ? styles.tile_container_open : styles.tile_container_close}>
+            <div className={styles.tile_icon}>{icon}</div>
+            {open && <div className={styles.tile_title}>{title}</div>}
+        </div>
+    );
+}
+
+const SideNav = () => {
+    const [open, setOpen] = useState(true);
+
+    const profile = { icon: <CgProfile />, title: "Profile" };
+    const group = { icon: <MdGroup />, title: "Groups" };
+    const settings = { icon: <MdSettings />, title: "Settings" };
+
+    return (
+        <div className={open ? styles.container_open : styles.container_close}>
+            <button className={styles.arrow} onClick={() => setOpen(!open)}>{ open? <MdArrowBack /> : <MdArrowForward />}</button>
+            <Tile {...profile} open={open} />
+            <Tile {...group} open={open} />
+            <Tile {...settings} open={open} />
+        </div>
+    );
+}
+
+export default SideNav;

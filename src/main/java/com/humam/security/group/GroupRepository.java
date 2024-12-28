e26d4123f8f2a304ad2b7e48b9a6445e5ab8be52
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-29 00:51:13 +0300

    query

--- src/main/java/com/humam/security/group/GroupRepository.java
+++ src/main/java/com/humam/security/group/GroupRepository.java
@@ -3,10 +3,20 @@ package com.humam.security.group;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import com.humam.security.user.User;
+import java.util.List;
+
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Integer> {
     
     Optional<Group> findById(Integer id);
+    List<Group> findByCreatedBy(User createdBy);    
+    
+    @Query("SELECT CASE WHEN g.createdBy.id = :userId THEN true ELSE false END FROM Group g WHERE g.id = :groupId")
+    boolean isGroupOwner(@Param("userId") Integer userId, @Param("groupId") Integer groupId);
+
 }

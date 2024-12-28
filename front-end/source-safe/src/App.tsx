9a2e7925d9a3ec6e1ee9d1eb4809efb532edce07
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-28 18:40:29 +0300

    ...

---
+++ front-end/source-safe/src/App.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import { BiRightArrow } from 'react-icons/bi';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Login from './Components/auth/Login'; 
+import Signup from './Components/auth/Signup'; 
+import ConfirmCode from './Components/auth/CofirmCode'; 
+import SideNav from './Components/navigation/sideNav'; 
+import './App.css';
+
+function Home() {
+  return (
+        <Link to="/login" className='link'>
+          <div className='Enter'> Enter <BiRightArrow className='arrow'/></div>
+        </Link>
+  );
+}
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/confirmcode" element={<ConfirmCode />} />
+          <Route path="/home" element={<SideNav />} />
+        </Routes>
+    </Router>
+  );
+}
+
+
+export default App;

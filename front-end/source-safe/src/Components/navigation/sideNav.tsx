5d7e452934f76ebcc0d15d32f68d35a712e2b222
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-30 00:07:19 +0300

    accidentally fixed things

--- front-end/source-safe/src/Components/navigation/sideNav.tsx
+++ front-end/source-safe/src/Components/navigation/sideNav.tsx
@@ -1,37 +1,45 @@
 import { useState } from "react";
-import { CgProfile } from "react-icons/cg"
+import { CgProfile } from "react-icons/cg";
 import { MdGroup, MdSettings, MdArrowBack, MdArrowForward } from "react-icons/md";
-import styles from './sideNav.module.css';
+import { Link } from "react-router-dom";
+import styles from "./sideNav.module.css";
 
 interface TileProps {
-    title: string;
-    icon: React.ReactNode;
+  title: string;
+  icon: React.ReactNode;
+  path: string;
 }
 
-const Tile = ({ title, icon, open }: TileProps & { open: boolean }) => {
-    return (
-        <div className={open ? styles.tile_container_open : styles.tile_container_close}>
-            <div className={styles.tile_icon}>{icon}</div>
-            {open && <div className={styles.tile_title}>{title}</div>}
-        </div>
-    );
-}
+const Tile = ({ title, icon, path, open }: TileProps & { open: boolean }) => {
+  return (
+    <Link to={path} className={styles.link}>
+      <div className={open ? styles.tile_container_open : styles.tile_container_close}>
+        <div className={styles.tile_icon}>{icon}</div>
+        {open && <div className={styles.tile_title}>{title}</div>}
+      </div>
+    </Link>
+  );
+};
 
 const SideNav = () => {
-    const [open, setOpen] = useState<boolean>(true);
+  const [open, setOpen] = useState<boolean>(true);
 
-    const profile = { icon: <CgProfile />, title: "Profile" };
-    const group = { icon: <MdGroup />, title: "Groups" };
-    const settings = { icon: <MdSettings />, title: "Settings" };
+  const menuItems = [
+    { icon: <CgProfile />, title: "Profile", path: "/profile" },
+    { icon: <MdGroup />, title: "Groups", path: "/groups" },
+    { icon: <MdSettings />, title: "Settings", path: "/settings" },
+  ];
 
-    return (
-        <div className={open ? styles.container_open : styles.container_close}>
-            <button className={styles.arrow} onClick={() => setOpen(!open)}>{ open? <MdArrowBack /> : <MdArrowForward />}</button>
-            <Tile {...profile} open={open} />
-            <Tile {...group} open={open} />
-            <Tile {...settings} open={open} />
-        </div>
-    );
-}
+  return (
+    <div className={open ? styles.container_open : styles.container_close}>
+      <button className={styles.arrow} onClick={() => setOpen(!open)}>
+        {open ? <MdArrowBack /> : <MdArrowForward />}
+      </button>
+      {menuItems.map((item) => (
+        <Tile key={item.title} {...item} open={open} />
+      ))}
+    </div>
+  );
+};
 
 export default SideNav;

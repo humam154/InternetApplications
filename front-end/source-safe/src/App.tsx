890ba6c22fd4f95aa911ba63f6f8e8bedde9c12a
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-30 00:05:03 +0300

    routing

--- front-end/source-safe/src/App.tsx
+++ front-end/source-safe/src/App.tsx
@@ -1,36 +1,43 @@
-import { useState } from 'react';
-import { BiRightArrow } from 'react-icons/bi';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BiRightArrow } from 'react-icons/bi';
 
-import Login from './Components/auth/Login'; 
-import Signup from './Components/auth/Signup'; 
-import ConfirmCode from './Components/auth/CofirmCode'; 
-import SideNav from './Components/navigation/sideNav'; 
-import './App.css';
+import Layout from "./Components/Layout";
+import Login from "./Components/auth/Login";
+import Signup from "./Components/auth/Signup";
+import ConfirmCode from "./Components/auth/CofirmCode";
+import GroupsPage from "./Components/group/GroupsPage";
+import styles from './App.module.css';
 
 function Home() {
   return (
-        <Link to="/login" className='link'>
-          <div className='Enter'> Enter <BiRightArrow className='arrow'/></div>
-        </Link>
+    <div className={styles.home}>
+      <Link to="/login" className={styles.link}>
+        <div className={styles.enter}>
+          Enter <BiRightArrow className={styles.arrow} />
+        </div>
+      </Link>
+    </div>
   );
 }
 
-function App() {
-  const [count, setCount] = useState(0);
-
+const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/confirmcode" element={<ConfirmCode />} />
-          <Route path="/home" element={<SideNav />} />
-        </Routes>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/confirmcode" element={<ConfirmCode />} />
+        <Route path="/home" element={<Home />} />
+        
+        <Route path="/" element={<Layout />}>
+          <Route path="groups" element={<GroupsPage />} />
+          <Route path="profile" element={<h1>Profile Page</h1>} />
+          <Route path="settings" element={<Login />} />
+        </Route>
+      </Routes>
     </Router>
   );
-}
-
+};
 
 export default App;

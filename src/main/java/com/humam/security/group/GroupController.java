90cc59d6eaa349a08d9290ab3e3804fe283bc58e
susalulmumaO12 <fawal.salma@gmail.com>
2024-12-29 16:24:12 +0300

    isOwner: simple boolean response

--- src/main/java/com/humam/security/group/GroupController.java
+++ src/main/java/com/humam/security/group/GroupController.java
@@ -3,17 +3,20 @@ package com.humam.security.group;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.humam.security.utils.GenericResponse;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 
 @CrossOrigin(origins = "http://localhost:5173")
@@ -24,6 +27,15 @@ public class GroupController {
 
     private final GroupSerivce groupSerivce;
 
+    @GetMapping("/isowner/{gid}")
+    public ResponseEntity<Boolean> isGroupOwne(
+        @RequestHeader("Authorization") String token,
+        @PathVariable @NotNull @Min(1) Integer gid
+        ) {
+
+        return ResponseEntity.ok(groupSerivce.isGroupOwner(token, gid));
+    }
+    
     @PostMapping("/create")
     public ResponseEntity<GenericResponse<CreateGroupResponse>> createGroup(
         @RequestHeader("Authorization") String token,

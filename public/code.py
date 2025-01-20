f1bca2a98af53d6f7fafe7100e7f0fddd28392c2
susalulmumaO12 <fawal.salma@gmail.com>
2025-01-20 22:06:14 +0300

    gitignore

--- public/code.py
+++
@@ -1,7 +0,0 @@
-from difflib import SequenceMatcher
-
-# Define two code strings
-code1 = """..."""
-code2 = """..."""
-similarity = SequenceMatcher(None, code1, code2).ratio() * 100
-print(f"Similarity: {similarity:.2f}%")
